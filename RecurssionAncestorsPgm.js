17366048a84bd95f968778c4bb3abf5d15311f24
SETHULAKSHMI-PM <sethulakshmi@razorthink.net>
2017-10-12 18:49:54 +0530

    Recurssion Array of Objects into Ancestors Tree Pgm

--- RecurssionAncestorsPgm.js
+++ RecurssionAncestorsPgm.js
@@ -2,6 +2,8 @@ const categories = [
   { id: 'E-commerce', parent: null },
   { id: 'Electronics', parent: 'E-commerce' },
   { id: 'Mobiles', parent: 'Electronics' },
+  { id: 'zzzzz', parent: 'Mobiles' },
+  { id: 'zzzzzzzzzz', parent: 'Mobiles' },
   { id: 'Laptops', parent: 'Electronics' },
   { id: 'Camera', parent: 'Electronics' },
   { id: 'Lifestyle', parent: 'E-commerce' },
@@ -11,29 +13,15 @@ const categories = [
   { id: 'Jewellery', parent: 'Lifestyle' },
 ]
 
-const createObject = function(data, parentId) {
-
-	var obj = {};
-	var childrenObj = [];
-	
-	if(parentId == undefined) {
-		childrenObj = data.filter((item) => item.parent == null);
-	}
-	else {
-		childrenObj = data.filter((item) => item.parent == parentId);
-	}
-	
-	if(childrenObj.length == 0) {
+const createObject = ((data, parentId) => {
+	//first time parentId will be undefined
+  let childObj = data.filter((line) => line.parent == parentId);
+  
+	const result = childObj.reduce((obj, value) => {
+    //passing current line id as next line's parent.
+		obj[value.id] = createObject(data, value.id)
 		return obj;
-	}
-	else {	
-		for(var i=0;i<childrenObj.length;i++)	{
-			var key = childrenObj[i].id;
-			obj[key] = createObject(data, childrenObj[i].id);
-		}
-	}
-	
-	return obj;
-}
-
-console.log("Resulttt : ", createObject(categories))
+	}, {});
+	return result;
+});
+console.log("Result : \n", createObject(categories));
